22eeee38742dbceb08fb0ff7a639201c546a09fe
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2016-05-24 22:21:54 +0200

    Update typings version

--- src/_references.d.ts
+++ src/_references.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/main.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 /// <reference path="../typings/node-creation-observer.d.ts" />
 /// <reference path="./Constants.ts" />
 /// <reference path="./Templates.ts" />
