3e4edce9a4737954ad8c8d52717d8c93b1e1df42
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2016-05-24 23:30:03 +0200

    Add node-creation-observer typing

--- src/_references.d.ts
+++ src/_references.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="../typings/index.d.ts" />
-/// <reference path="../typings/node-creation-observer.d.ts" />
 /// <reference path="./Externals.ts" />
 /// <reference path="./Templates.ts" />
